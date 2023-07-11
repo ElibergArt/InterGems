696f8f35ae82d0afbb05dfeb476af567118750ce
ElibergArt <morriel666@gmail.com>
2023-07-11 16:12:59 +0300

    настройки index.php
    настроена тема для index.php

--- v2/template-v2.php
+++ v2/template-v2.php
@@ -6,6 +6,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Finlandica:wght@500&family=Montserrat&display=swap" rel="stylesheet">
     <title>InterGems -</title>
 
 
@@ -16,27 +20,34 @@
 
 </body>
 <div class="header">
-    <div class="header_container">
+    <div class="container menu">
         <div class="header_inner">
             <div class="header_logo">
                 <a href="#"></a>
                 <img src="logo.svg" alt="">
-            </div>
 
+            </div>
             <nav class="navbar">
-                <a class="nav-link" href="#">О нас</a>
+                <a class="nav-link" href="index.php">О нас</a>
                 <a class="nav-link" href="#">Каталог</a>
                 <a class="nav-link" href="#">Мероприятия</a>
                 <a class="nav-link" href="#">Контакты</a>
             </nav>
         </div>
     </div>
-    <div class="header_container">
-        <div class="header_title">
-            <h1 class="intro_title">InterGems</h1>
-            <h2>Премиальная огранка и потрясающее качество</h2>
+
+</div>
+<div class="intro">
+    <div class="container">
+        <div class="intro_title">
+            <h1 class="intro_bigtitle">InterGems</h1>
+            <h2 class="intro_stitle">Премиальная огранка и потрясающее качество</h2>
         </div>
     </div>
+</div>
+<div class="page_content">
+    <?php echo $content 
+    ?>
 </div>
 <footer>
 
